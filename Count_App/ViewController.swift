94ed5b74827104fa7c3427c56a82745523edda24
Yutaro <yutaro@YutaronoMac.local>
2015-04-24 19:06:45 +0900

    Initial Commit

---
+++ Count_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count_App
+//
+//  Created by Yutaro on 2015/04/24.
+//  Copyright (c) 2015年 Yutaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
